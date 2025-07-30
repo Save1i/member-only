b0e627c57c247801d0162f8fe5396accab3e6566
Save1i <savva0512@gmail.com>
2025-07-30 16:25:27 +0300

    add: express, basic settings

---
+++ server.ts
@@ -0,0 +1,13 @@
+import {config} from "dotenv"
+import express, { Request, Response } from "express"
+import router from "routes/index"
+
+config()
+const app = express()
+app.use(express.json());
+
+app.use("/", router)
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
