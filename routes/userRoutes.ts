f115090d80bf76f36a658bfd98e40afe99fb3a51
Save1i <savva0512@gmail.com>
2025-07-30 16:26:23 +0300

    add: routes

---
+++ routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import {Router} from "express"
+import userControllers from "controllers/userControllers"
+
+const router = Router()
+
+router.post("sign-up", userControllers.createUserPost)
+router.get("/", userControllers.getAllUsers)
+
+export default router
