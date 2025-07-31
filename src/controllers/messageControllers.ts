d92eb3c99bdf7949995e530ca9d0cd77131e15f6
Save1i <savva0512@gmail.com>
2025-07-31 20:28:27 +0300

    add: inser message

--- src/controllers/messageControllers.ts
+++ src/controllers/messageControllers.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express"
 import query from "../queries/queries"
 
+interface PassportSession {
+  passport?: {
+    user: number;
+  };
+}
+
 async function getAllMessages(req: Request, res: Response) {
     console.log(req.isAuthenticated())
     const messages = await query.messageGet(req.isAuthenticated())
-    console.log(messages)
     res.render("messageBoard", {messages})
 }
 
+async function createMessagePost(req: Request, res: Response) {
+    const {title, message} = req.body
+    const session = req.session as PassportSession;
+    const id = session.passport?.user as number // временно
+    await query.messagePost(title, message, id)
+    res.redirect("/message")
+}
+
+async function createMessageGet(req: Request, res: Response) {
+    res.render("messageForm")
+}
+
+
 export default {
     getAllMessages,
+    createMessagePost,
+    createMessageGet,
 }
