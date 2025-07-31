c9f3794266fdc635b487294ffd675c9bc81c91b2
Save1i <savva0512@gmail.com>
2025-07-31 16:17:04 +0300

    add: routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,25 @@
 import {Router} from "express"
 import userControllers from "@/controllers/userControllers"
+import passport from "passport"
 
 const router = Router()
 
-router.post("sign-up", userControllers.createUserPost)
+router.post("/sign-up", userControllers.createUserPost)
+router.get("/sign-up", userControllers.createUserGet)
+router.get("/log-in", userControllers.loginUserGet)
+router.post("/login", passport.authenticate('local', {
+    successRedirect: '/message',
+    failureRedirect: '/user/login',
+    failureFlash: false
+}))
+router.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/message");
+  });
+});
 router.get("/", userControllers.getAllUsers)
 
 export default router
