2ad71b7c74a5c5478e860939b98d843f72a3ac91
Save1i <savva0512@gmail.com>
2025-07-31 16:16:25 +0300

    add: controllers

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,18 +1,36 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import query from "../queries/queries"
+import bcrypt from "bcrypt"
+import passport from "passport"
 
 async function createUserPost(req: Request, res: Response) {
-    const {firstName, lastName, nickname, password} = req.body
-    await query.insertUser(firstName, lastName, nickname, password)
-    res.json({firstName, lastName, nickname, password})
+    const {firstName, lastName, nickName, password} = req.body
+    const hashPasword = await bcrypt.hash(password, 10)
+    await query.insertUser(firstName, lastName, nickName, hashPasword)
+    res.json({firstName, lastName, nickName, password})
+}
+
+async function createUserGet(req: Request, res: Response) {
+    res.render('registration', {req})
+}
+
+async function loginUserGet(req: Request, res: Response) {
+    res.render('login', {req})
 }
 
 async function getAllUsers(req: Request, res: Response) {
-    const users = await query.getAllUsers()
-    res.json(users)
+    if(req.isAuthenticated()) {
+        const users = await query.getAllUsers()
+        console.log(users)
+        return users
+    } else {
+        res.redirect("/user/login")
+    }
 }
 
 export default {
     createUserPost,
+    createUserGet,
     getAllUsers,
+    loginUserGet,
 }
