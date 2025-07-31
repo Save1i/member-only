727255e40464b7436c3f61fe7689b7c1ea05bd21
Save1i <savva0512@gmail.com>
2025-07-31 16:16:54 +0300

    add: message route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from "express"
 import userRouter from "./userRoutes"
+import messageRouter from "./messageRoutes"
 
 const router = Router()
 
 router.use("/user", userRouter)
+router.use("/message", messageRouter)
 
 export default router
