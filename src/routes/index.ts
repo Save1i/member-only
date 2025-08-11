fc0fbc233cfd3352a828137ebd532aa154ea20cb
Save1i <savva0512@gmail.com>
2025-08-11 11:19:40 +0300

    add: basic route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from "express"
 import userRouter from "./userRoutes"
 import messageRouter from "./messageRoutes"
+import basicRouter from "./basicRouter"
 
 const router = Router()
 
+router.use("/", basicRouter)
 router.use("/user", userRouter)
 router.use("/message", messageRouter)
 
